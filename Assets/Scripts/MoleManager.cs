9f5550b41ef75d64a75683d0a3a7a8ce36afc1c4
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-29 12:38:05 +1000

    added the instantiate

---
+++ Assets/Scripts/MoleManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleManager : MonoBehaviour
+{
+    public GameObject[] holes;
+    public GameObject mole;
+    private float Spawn;
+    private int rando;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Spawn += Time.deltaTime;
+        if (Spawn >= 0)
+        {
+            rando = Random.Range(0, holes.Length - 1);
+            Instantiate(mole, holes[rando].transform.position, Quaternion.identity);
+            Spawn = -1;
+        }
+    }
+}
